42b365a12f914ed09e3954b30ac56426d7c90499
chikati3 <chikazawakazuhiro3@gmail.com>
2023-02-02 17:17:25 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,18 @@
+// 投稿関数
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   //リクエストを送信する処理
   // idの名前指定
@@ -17,6 +32,23 @@ function post (){
     XHR.responseType = "json";
     // リクエスト送信
     XHR.send(formData);
+
+    // レスポンス受信
+    XHR.onload = () => {
+      // ステータスコード 200成功 200以外エラー
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+
+      const list = document.getElementById("list");
+      // フォーム内容を変数へ
+      const formText = document.getElementById("content");
+      // htmlの要素に挿入
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      // フォーム内リセット 連続投稿不可
+      formText.value = "";
+    };
   });
 };
  
